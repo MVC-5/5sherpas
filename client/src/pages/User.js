111cd1db97e02f799380b72b721f81e248c4c4c7
ShepLT1 <lshepherd234@gmail.com>
2020-11-03 18:33:37 -0800

    Added front end API route comments, fixed src index.js to use strict

--- client/src/pages/User.js
+++ client/src/pages/User.js
@@ -4,62 +4,56 @@ import { Container } from "../components/Grid";
 
 function User() {
 
-  // Loads all books and sets them to books
-  function loadUserRoutes() {
-    API.getUser()
-      .then(res =>
-        console.log(res)
-      )
-      .catch(err => console.log(err));
-    API.getUserSettings(1)
-      .then(res =>
-        console.log(res)
-      )
-      .catch(err => console.log(err));
-    API.updateUserSettings(1)
-      .then(res =>
-        console.log(res)
-      )
-      .catch(err => console.log(err));
-    API.saveUser({})
-      .then(res =>
-        console.log(res)
-      )
-      .catch(err => console.log(err));
-    API.getDashData(1)
-      .then(res =>
-        console.log(res)
-      )
-      .catch(err => console.log(err));
-    API.getNewChallenge()
-      .then(res =>
-        console.log(res)
-      )
-      .catch(err => console.log(err));
-    API.getMovieSugg()
-      .then(res =>
-        console.log(res)
-      )
-      .catch(err => console.log(err));
-    API.getPhysActSugg()
-      .then(res =>
-        console.log(res)
-      )
-      .catch(err => console.log(err));
-    API.getMentalActSugg()
-      .then(res =>
-        console.log(res)
-      )
-      .catch(err => console.log(err));
-    API.updateChall()
-      .then(res =>
-        console.log(res)
-      )
-      .catch(err => console.log(err));
-  };
-
-  loadUserRoutes()
-
+  API.getUser()
+    .then(res =>
+      console.log(res)
+    )
+    .catch(err => console.log(err));
+  API.getUserSettings(1)
+    .then(res =>
+      console.log(res)
+    )
+    .catch(err => console.log(err));
+  API.updateUserSettings(1)
+    .then(res =>
+      console.log(res)
+    )
+    .catch(err => console.log(err));
+  API.saveUser({})
+    .then(res =>
+      console.log(res)
+    )
+    .catch(err => console.log(err));
+  API.getDashData(1)
+    .then(res =>
+      console.log(res)
+    )
+    .catch(err => console.log(err));
+  API.getNewChallenge()
+    .then(res =>
+      console.log(res)
+    )
+    .catch(err => console.log(err));
+  API.getMovieSugg()
+    .then(res =>
+      console.log(res)
+    )
+    .catch(err => console.log(err));
+  API.getPhysActSugg()
+    .then(res =>
+      console.log(res)
+    )
+    .catch(err => console.log(err));
+  API.getMentalActSugg()
+    .then(res =>
+      console.log(res)
+    )
+    .catch(err => console.log(err));
+  API.updateChall()
+    .then(res =>
+      console.log(res)
+    )
+    .catch(err => console.log(err));
 
   return (
     <Container>
