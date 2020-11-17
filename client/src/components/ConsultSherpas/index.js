0333d44e134f3f35522c79475ee1d9c1da449af2
Josh Zappone <zappone.josh@gmail.com>
2020-11-16 23:12:58 -0800

    some consulting sherpa functionality but mostly style changes for responsiveness

--- client/src/components/ConsultSherpas/index.js
+++ client/src/components/ConsultSherpas/index.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { Grid } from "semantic-ui-react";
 import "./style.css";
 import mainKnot from "../../assets/main-knot.png";
 import sherpa1 from "../../assets/sherpa1.png";
@@ -19,16 +20,38 @@ function ConsultSherpas() {
   const [bubble2Text, setBubble2Text] = useState(
     "Click on me to get a physical activity suggestion"
   );
-  const [bubble3Text, setBubble3Text] = useState(
+  const [bubble1Text, setBubble1Text] = useState(
     "Click on me to get a mental activity suggestion"
   );
+  const [movieList, setMovieList] = useState([]);
+  const [genre, setGenre] = useState(null);
+  // const [physList, setPhysList] = useState([]);
+  // const [mentalList, setMentalList] = useState([]);
+
+  // this is a mess
+  async function getMovie(movieGenre) {
+    if (movieList.length && movieGenre === genre) {
+      const randomIndex = Math.floor(Math.random() * movieList.length);
+      return movieList[randomIndex];
+    } else {
+      // this is running every time :(
+      setGenre(movieGenre);
+      await API.getMovieSugg(movieGenre).then((res) => {
+        setMovieList(res.data);
+        console.log(res);
+        const randomIndex = Math.floor(Math.random() * res.data.length);
+        const movie = res.data[randomIndex];
+        // this is returning
+        return movie;
+      });
+    }
+  }
 
   function handleSherpaClick(id) {
     console.log(id);
     if (id === "Josh") {
-      API.getMovieSugg("Action").then((res) => {
-        console.log(res);
-      });
+      const newMovie = getMovie("Action");
+      console.log(newMovie);
     }
 
     if (id === "Jon") {
@@ -45,7 +68,7 @@ function ConsultSherpas() {
       API.getMentalActSugg().then((res) => {
         mentalAct =
           res.data[Math.floor(Math.random() * res.data.length) + 1].name;
-        setBubble3Text(mentalAct);
+        setBubble1Text(mentalAct);
         console.log(mentalAct);
       });
     }
@@ -54,53 +77,74 @@ function ConsultSherpas() {
     <>
       <h1 className="header">consult a sherpa</h1>
       <div className="knot-container">
-        <button onClick={(e) => handleSherpaClick(e.target.id)}>
-          {" "}
-          <img
-            className="sherpa-img s-10"
-            id="Caleb"
-            src={sherpa5}
-            alt="sherpa5"
-          />
-        </button>
-        <button onClick={(e) => handleSherpaClick(e.target.id)}>
-          {" "}
-          <img
-            className="sherpa-img s-8"
-            id="Jon"
-            src={sherpa1}
-            alt="sherpa1"
-          />
-        </button>
-        <button onClick={(e) => handleSherpaClick(e.target.id)}>
-          {" "}
-          <img
-            className="sherpa-img s-9"
-            id="Josh"
-            src={sherpa3}
-            alt="sherpa3"
-          />
-        </button>
-        <button onClick={(e) => handleSherpaClick(e.target.id)}>
-          {" "}
-          <img className="yak-img s-yak" src={yak1} id="PC Yak" alt="yak1" />
-        </button>
+        <Grid columns={3} stackable id="sherpa-set">
+          <Grid.Column>
+            <Grid.Row className="consult-container">
+              {/* mental act sherpa */}
+              <div className="bubble-container">
+                <img className="bubble" src={bubble1} alt="bubble" />
 
-        <div>
-          <img className="bubble-img bubble-1" src={bubble1} alt="bubble" />
-        </div>
-        <div className="bubble-2">
-          <p>{bubble2Text}</p>
-        </div>
-        <img className="bubble-img bubble-2" src={bubble2} alt="bubble" />
+                <p className="bubble-text">{bubble1Text}</p>
+              </div>
+              <img
+                onClick={(e) => handleSherpaClick(e.target.id)}
+                aria-hidden="true"
+                className="consult-sherpa"
+                id="Caleb"
+                src={sherpa5}
+                alt="sherpa5"
+              />
+            </Grid.Row>
+          </Grid.Column>
+          <Grid.Column>
+            <Grid.Row className="consult-container">
+              <div className="bubble-container">
+                <img className="bubble" src={bubble2} alt="bubble" />
 
-        <div className="bubble-3">
-          <p>{bubble3Text}</p>
-        </div>
-        <img className="bubble-img bubble-3" src={bubble3} alt="bubble" />
+                <p className="bubble-text">{bubble2Text}</p>
+              </div>
+              {/* phyiscal act sherpa */}
+              <img
+                aria-hidden="true"
+                onClick={(e) => handleSherpaClick(e.target.id)}
+                className="consult-sherpa"
+                id="Jon"
+                src={sherpa1}
+                alt="sherpa1"
+              />
+            </Grid.Row>
+          </Grid.Column>
+          <Grid.Column>
+            <Grid.Row className="consult-container">
+              <div className="bubble-container">
+                <img className="bubble" src={bubble3} alt="bubble" />
 
-        <div>
-          <img className="bubble-img bubble-4" src={bubble4} alt="bubble" />
+                <p className="bubble-text">Content</p>
+                {/* <p>{bubble3Text}</p> */}
+              </div>
+              {/* movie sherpa */}
+              <img
+                aria-hidden="true"
+                onClick={(e) => handleSherpaClick(e.target.id)}
+                className="consult-sherpa"
+                id="Josh"
+                src={sherpa3}
+                alt="sherpa3"
+              />
+            </Grid.Row>
+          </Grid.Column>
+        </Grid>
+
+        <div className="yak-container">
+          <img className="bubble-4" src={bubble4} alt="bubble" />
+          <img
+            onClick={(e) => handleSherpaClick(e.target.id)}
+            aria-hidden="true"
+            className="yak-img"
+            src={yak1}
+            id="PC Yak"
+            alt="yak1"
+          />
         </div>
         <div>
           <img id="second-knot" src={mainKnot} alt="knot-logo" />
--- dbSeed/movieseed.js
+++ dbSeed/movieseed.js
@@ -1,5 +1,7 @@
 let mongoose = require("mongoose");
 let db = require("../models");
+const dotenv = require("dotenv");
+dotenv.config();
 
 mongoose.connect(
   process.env.MONGODB_URI || "mongodb://localhost/5sherpas",
