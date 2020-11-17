9af64599d1807babe4966ea2937827b4f94736d9
Josh Zappone <zappone.josh@gmail.com>
2020-11-16 19:28:13 -0800

    d3 graph with user data and no repetitions

--- client/src/components/D3GraphTest/index.js
+++ client/src/components/D3GraphTest/index.js
@@ -1,158 +1,154 @@
-import React, { useEffect, useRef } from 'react';
-import * as d3 from 'd3';
+import React, { useEffect, useRef, useState, useContext } from "react";
+import * as d3 from "d3";
+import UserContext from "../../utils/UserContext";
 
-
-export const D3GraphTest = (props) => {
+const D3GraphTest = () => {
+  const { progressData } = useContext(UserContext);
 
   const d3Container = useRef(null);
+  const [totalProgress, setTotalProgress] = useState([]);
 
-  useEffect(() => {
-      
-    // const totalProgress = [
-    //   15, 38, 40, 25,
-    // ];
-
-    const totalProgress = props.progressData.map(each => each.completed)
-        
+  function makeGraph(totalProgress) {
     // Setting up margins
     const margin = {
       top: 50,
       right: 25,
       bottom: 25,
-      left: 50
-    }
-          
+      left: 50,
+    };
+
     // Graph area dimensions
     const height = 500 - margin.top - margin.bottom;
     const width = 450 - margin.left - margin.right;
-      
+
     // Mouse over dynamic color
     let dynamicColor;
-      
+
     // Proportional height
-    const yScale = 
-    d3.scaleLinear() 
+    const yScale = d3
+      .scaleLinear()
       .domain([0, d3.max(totalProgress)])
       .range([0, height]);
-      
+
     // Proportional width
-    const xScale = 
-    d3.scaleBand()
+    const xScale = d3
+      .scaleBand()
       .domain(d3.range(0, totalProgress.length))
       .range([0, width])
       .paddingInner(0.2)
       .paddingOuter(1);
-      
+
     // Multicolor using scaleLinear
-    const colors =
-    d3.scaleLinear()
+    const colors = d3
+      .scaleLinear()
       .domain([
-        0, 
-        totalProgress.length * .33, 
-        totalProgress.length * .66, 
-        totalProgress.length
+        0,
+        totalProgress.length * 0.33,
+        totalProgress.length * 0.66,
+        totalProgress.length,
       ])
-      .range([
-        '#d6e9c6', 
-        '#bce8f1', 
-        '#faebcc', 
-        '#ebccd1'
-      ]);
-          
+      .range(["#d6e9c6", "#bce8f1", "#faebcc", "#ebccd1"]);
+
     // Main graph using d3 api references
-    const awesome = 
-    d3.select(d3Container.current)
-      .append('svg')
-      .attr('width', width + margin.left + margin.right)
-      .attr('height', height + margin.top + margin.bottom)
-      .style('background', 'transparent')
-      .append('g')
-      .attr(
-        'transform', 
-        'translate(' + margin.left + ', ' + margin.top + ')'
-      )
-      .selectAll('rect')
+    const awesome = d3
+      .select(d3Container.current)
+      .append("svg")
+      .attr("width", width + margin.left + margin.right)
+      .attr("height", height + margin.top + margin.bottom)
+      .style("background", "transparent")
+      .append("g")
+      .attr("transform", "translate(" + margin.left + ", " + margin.top + ")")
+      .selectAll("rect")
       .data(totalProgress)
       .enter()
-      .append('rect')
-      .style('fill', (data, i) => {
+      .append("rect")
+      .style("fill", (data, i) => {
         return colors(i);
-      },)
-      .attr('width', xScale.bandwidth())
-      .attr('x', function(data, i) {
+      })
+      .attr("width", xScale.bandwidth())
+      .attr("x", function (data, i) {
         return xScale(i);
       })
-      .attr('height', 0)
-      .attr('y', height)
-      
-    // Mouse over dynamic color
-      .on('mouseover', function() {
+      .attr("height", 0)
+      .attr("y", height)
+
+      // Mouse over dynamic color
+      .on("mouseover", function () {
         dynamicColor = this.style.fill;
-        d3.select(this)
-          .style('fill', '#3c763d')
+        d3.select(this).style("fill", "#3c763d");
       })
-      .on('mouseout', function() {
-        d3.select(this)
-          .style('fill', dynamicColor)
-      })
-      
-      // Graph animated transition
-      awesome.transition()
-      .attr('height', (data) => {
+      .on("mouseout", function () {
+        d3.select(this).style("fill", dynamicColor);
+      });
+
+    // Graph animated transition
+    awesome
+      .transition()
+      .attr("height", (data) => {
         return yScale(data);
       })
-      .attr('y', (data) => {
+      .attr("y", (data) => {
         return height - yScale(data);
       })
       .delay((data, i) => {
         return i * 20;
       })
-      .duration(3000)
-      .ease(d3.easeElastic)
-      
-      // Vertical guide / challenges
-      const verticalGuideScale = 
-      d3.scaleLinear()
-        .domain([0, d3.max(totalProgress)])
-        .range([height, 0])
-      
-      const vAxis = 
-      d3.axisLeft(verticalGuideScale)
-        .ticks(10)
-      
-      const verticalGuide = d3.select('svg').append('g')
-      vAxis(verticalGuide)
-      verticalGuide.attr(
-        'transform', 
-        'translate(' + margin.left + ', ' + margin.top + ')'
-      )
-      verticalGuide.selectAll('path').style('stroke', '#00c4ff');
-      verticalGuide.selectAll('line').style('stroke', '#00c4ff');
-            
-      // Horizontal guide / weeks
-      const hAxis = 
-      d3.axisBottom(xScale)
-        .ticks(totalProgress.size)
-      
-      const horizontalGuide = d3.select('svg').append('g')
-      hAxis(horizontalGuide)
-      horizontalGuide.attr(
-        'transform', 
-        'translate(' + margin.left + ', ' + (height + margin.top) + ')'
-      );
-      horizontalGuide.selectAll('path').style('stroke', '#00c4ff');
-      horizontalGuide.selectAll('line').style('stroke', '#00c4ff');
+      .duration(700)
+      .ease(d3.easeElastic);
 
-      awesome.selectAll('*').remove()
-  });
+    // Vertical guide / challenges
+    const verticalGuideScale = d3
+      .scaleLinear()
+      .domain([0, d3.max(totalProgress)])
+      .range([height, 0]);
 
+    const vAxis = d3.axisLeft(verticalGuideScale).ticks(10);
+
+    const verticalGuide = d3.select("svg").append("g");
+    vAxis(verticalGuide);
+    verticalGuide.attr(
+      "transform",
+      "translate(" + margin.left + ", " + margin.top + ")"
+    );
+    verticalGuide.selectAll("path").style("stroke", "#00c4ff");
+    verticalGuide.selectAll("line").style("stroke", "#00c4ff");
+
+    // Horizontal guide / weeks
+    const hAxis = d3.axisBottom(xScale).ticks(totalProgress.size);
+
+    const horizontalGuide = d3.select("svg").append("g");
+    hAxis(horizontalGuide);
+    horizontalGuide.attr(
+      "transform",
+      "translate(" + margin.left + ", " + (height + margin.top) + ")"
+    );
+    horizontalGuide.selectAll("path").style("stroke", "#00c4ff");
+    horizontalGuide.selectAll("line").style("stroke", "#00c4ff");
+  }
+
+  useEffect(() => {
+    // if (totalProgress.length) {
+    //   makeGraph(totalProgress);
+    // }
+    // const totalProgress = progressData.map((each) => each.completed);
+  }, []);
+
+  useEffect(() => {
+    document.querySelector("#d3Graph").innerHTML = "";
+
+    const newData = progressData.map((each) => each.completed);
+    setTotalProgress(newData);
+    console.log(progressData);
+    if (totalProgress.length) {
+      makeGraph(totalProgress);
+    }
+  }, [progressData]);
 
   return (
     <>
-      <div ref = {d3Container} width = '450' height = '500'></div>
+      <div id="d3Graph" ref={d3Container} width="450" height="500"></div>
     </>
-  )
-
-}
+  );
+};
 
 export default D3GraphTest;
--- client/src/components/WeeklyProgress/index.js
+++ client/src/components/WeeklyProgress/index.js
@@ -1,25 +1,23 @@
-import React, { useContext } from "react";
-import UserContext from "../../utils/UserContext";
+import React from "react";
 import "./style.css";
 
 import sherpa2 from "../../assets/sherpa2.png";
-import D3GraphHorizontal from "../D3GraphHorizontal";
+// import D3GraphHorizontal from "../D3GraphHorizontal";
 // import D3GraphVertical from "../D3GraphVertical";
-// import D3GraphTest from "../D3GraphTest";
+import D3GraphTest from "../D3GraphTest";
 
 export function WeeklyProgress() {
-  const { progressData } = useContext(UserContext);
   return (
     <>
       <div className="dash-section-1">
         <img className="sherpa-img-smaller" src={sherpa2} alt="sherpa4" />
-        <h1 className="section-title">weekly progress</h1>
+        <h1 className="section-title">your progress</h1>
       </div>
-      
+
       <div>
-        < D3GraphHorizontal progressData={progressData}/>
-        {/* < D3GraphVertical progressData={progressData}/> */}
-        {/* < D3GraphTest progressData={progressData}/> */}
+        {/* < D3GraphHorizontal progressData={progressData}/> */}
+        {/* <D3GraphVertical progressData={progressData} /> */}
+        <D3GraphTest />
       </div>
     </>
   );
