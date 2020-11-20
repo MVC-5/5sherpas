a31a0683e01e30ae09072a1cbaf6e6355c8a0d03
Josh Zappone <zappone.josh@gmail.com>
2020-11-20 14:28:15 -0800

    try catch all in routes file

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,11 @@ const apiRoutes = require("./api");
 router.use("/api", apiRoutes);
 
 // If no API routes are hit, send the React app
-router.use(function (req, res) {
+// router.use(function (req, res) {
+//   res.sendFile(path.join(__dirname, "../../client/build/index.html"));
+// });
+
+router.route("*").get((req, res) => {
   res.sendFile(path.join(__dirname, "../../client/build/index.html"));
 });
 
